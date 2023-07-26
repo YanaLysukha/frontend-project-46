83b3522d882ac13f42a34f7dbc064df3beeb39ab
YanaLysukha <yanina.lysukha@gmail.com>
2023-07-26 17:16:13 +0300

    Added changes to gendiff.js

--- gendiff.js
+++ gendiff.js
@@ -3,7 +3,7 @@ import { program } from 'commander';
 
 program
   .name('gendiff')
-  .description('Compares two configuration files and shows a difference')
+  .description('Compares two configuration files and shows a difference.')
   .version('1.0.0');
 
 program.parse();
