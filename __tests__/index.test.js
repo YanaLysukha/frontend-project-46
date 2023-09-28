94cc0eb6660ce4f227ebe256bfaef4c21e0ba8a9
YanaLysukha <yanina.lysukha@gmail.com>
2023-09-28 16:02:37 +0300

    tests added

---
+++ __tests__/index.test.js
@@ -0,0 +1,18 @@
+import { fileURLToPath } from 'url';
+import { readFileSync } from 'fs';
+import path, { dirname } from 'path';
+import genDiff from '../src';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filepath) => path.join(__dirname, '..', '__fixtures__', filepath);
+const readFile = (filepath) => readFileSync(getFixturePath(filepath), 'utf-8');
+
+const expectedStylish = readFile('expectedStylish.txt');
+
+test('genDiff-test', () => {
+  const fileBefore = '__fixtures__/file1.json';
+  const fileAfter = '__fixtures__/file2.json';
+  expect(genDiff(fileBefore, fileAfter)).toEqual(expectedStylish);
+});
